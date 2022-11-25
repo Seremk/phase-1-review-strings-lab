2bb07fbdb1d0832058bc1bce58117e4df96deab9
seremshadrack <skemei660@gmail.com>
2022-11-25 16:19:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser="Shadrack Serem";
+const welcomeMessage="Welcome to Flatbook, Shadrack Serem!";
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, SHADRACK SEREM!";
+const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
+
